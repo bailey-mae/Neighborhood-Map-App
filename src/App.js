6c72d85d939a2665ab107e70b6cf6d07558d91b6
Laura Bailey <lauramginn@gmail.com>
2018-12-08 15:50:34 -0500

    updated map component to work with react, no external dependency

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import './App.css';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+class App extends Component {
 
-export class MapContainer extends Component {
-  render() {
+  componentDidMount() {
+    this.renderMap ()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAGlDDI4IhHVNMooY1WCGtTb6TSGmRnv9Q&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+          const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: 39.4219709802915, lng: -77.4121168197085},
+            zoom: 13
+          });
+        }
+
+
+  render () {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 39.4219709802915,
-         lng: -77.4121168197085
-        }}
-      />
-    );
+      <main>
+      <div id="map"></div>
+      </main>
+    )
   }
 }
 
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAGlDDI4IhHVNMooY1WCGtTb6TSGmRnv9Q'
-})(MapContainer);
+
+function loadScript (url) {
+  var index =
+    window.document.getElementsByTagName("script")
+    [0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+export default App;
+
