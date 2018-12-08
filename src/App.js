34580141275b532b11b41abbada211ca461fa955
Laura Bailey <lauramginn@gmail.com>
2018-12-08 13:47:35 -0500

    "Created map component and added geocode location for Frederick,MD"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class App extends Component {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 39.4219709802915,
+         lng: -77.4121168197085
+        }}
+      />
     );
   }
 }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAGlDDI4IhHVNMooY1WCGtTb6TSGmRnv9Q'
+})(MapContainer);
