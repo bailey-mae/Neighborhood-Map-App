3d860dc2860312e3d91d90c7167f786639251729
Laura Bailey <lauramginn@gmail.com>
2018-12-08 17:40:28 -0500

    Added axios request and Foursquare API for venue search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
   componentDidMount() {
     this.renderMap ()
+    this.getVenues ()
   }
 
   renderMap = () => {
@@ -12,12 +14,32 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "1FSWOD3MHOO1XYMP5ESSXGDRJHEWMD3QMZWQIWYMA0G433C5",
+      client_secret: "FIJNBNPGLBMLRL0HY52UVXUIQF2KOHISCKOUVXB4OLFCH3JJ",
+      query: "food",
+      near: "Frederick",
+      v: "20182507"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        console.log(response.data.response.groups[0].items)
+      })
+      .catch(error => {
+        console.log("So sorry, the following error occured " + error)
+
+      })
+  }
+
   initMap = () => {
           const map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: 39.4219709802915, lng: -77.4121168197085},
             zoom: 13
           });
-        }
+  }
 
 
   render () {
